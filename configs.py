ce93d4a62e6506517a00d6703b99d31791183c3a
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-09-05 02:07:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "22620068"))
+    API_HASH = getenv("API_HASH", "11e2c113078324f7e36688baa86c3911")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7274390732:AAHq3xQfTtkckKAe-7vdkeYcyb4GkpY0QPY")
+    FSUB = getenv("FSUB", "xyz_bots")
+    CHID = int(getenv("CHID", "-1002186382302"))
+    SUDO = list(map(int, getenv("7229906119").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://t54s2lqiv6:2mOV4n1iL21cMcMH@cluster0.ma3sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
