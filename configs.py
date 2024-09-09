be764756927887867cc6bf6567723afa0e64b1ec
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-09-09 15:45:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22620068"))
     API_HASH = getenv("API_HASH", "11e2c113078324f7e36688baa86c3911")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7274390732:AAHq3xQfTtkckKAe-7vdkeYcyb4GkpY0QPY")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7025776766:AAHzLIPGQ7GpYJTA12b_kx9yYIGcG0lmO6s")
     FSUB = getenv("FSUB", "xyz_bots")
     CHID = int(getenv("CHID", "-1002186382302"))
     SUDO = 72299906119
